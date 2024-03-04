1b62df375f5840a2cf47d8df0024773d8bc13aaa
hanh42 <dinhh7844@gmail.com>
2024-03-05 00:41:20 +0700

    2024/3/5 12:41AM

---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { ActivityIndicator, Image, StyleSheet, View } from 'react-native'
+import { appColors } from '../constants/appColors'
+import { appInfo } from '../constants/appInfos'
+
+const SplashScreen = () => {
+    return (
+        <View style={{ flex: 1, backgroundColor: appColors.white, justifyContent: 'center', alignItems: 'center' }}>
+            {/* Image */}
+            <Image
+                style={styles.image}
+                resizeMode='contain'
+                source={require('../assets/images/logo.png')}
+            />
+            {/* activity */}
+            <View style={styles.wrapperActivity}>
+                <ActivityIndicator size={40} color={appColors.primary} />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: appInfo.size.WIDTH * 0.7,
+    },
+    wrapperActivity: {
+        position: 'absolute',
+        top: appInfo.size.HEIGHT * 0.6
+    }
+})
+export default SplashScreen
